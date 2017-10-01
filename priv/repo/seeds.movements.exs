dff2eca7200dbe059b121820447ea8c298e20010
May Pongpitpitak <me@mayppong.com>
2017-10-01 16:34:39 -0700

    Add sample movement data and how it could be used.

---
+++ priv/repo/seeds.movements.exs
@@ -0,0 +1,50 @@
+[
+  {"Squat", "", [
+    {"Front Squat", "with the bar in the rack position, squat down until your hips is at knee's level or below", [
+      {"Paused Front Squat", "", []}
+    ]},
+    {"Back Squat", "", [
+      {"Paused Back Squat", "", []}
+    ]},
+    {"Overhead Squat", "", [
+      {"Paused Overhead Squat", "", []}
+    ]}
+  ]},
+  {"Snatch", "", [
+    {"Squat Snatch", "", [
+      {"Hang Squat Snatch", "", []},
+      {"Paused Squat Snatch", "", []},
+    ]},
+    {"Power Snatch", "", [
+      {"Hang Power Snatch", "", []},
+      {"Paused Power Snatch", "", []},
+    ]},
+  ]},
+  {"Clean", "", [
+    {"Squat Clean", "", [
+      {"Hang Squat Clean", "", []},
+      {"Paused Squat Clean", "", []},
+    ]},
+    {"Power Clean", "", [
+      {"Hang Power Clean", "", []}
+      {"Paused Power Clean", "", []}
+    ]},
+  ]},
+  {"Jerk", "", [
+    {"Push Jerk", "", []},
+    {"Split Jerk", "", []},
+  ]},
+  {"Press", "", [
+    {"Shoulder Press", "", []},
+    {"Push Press", "", []},
+    {"Bench Press", "", []}
+  ]},
+  {"Deadlift", "", [
+    {"Conventional Deadlift", "", [
+      {"Deficit Conventional Deadlift", "", []}
+    ]},
+    {"Romanian Deadlift", "", [
+      {"Deficit Romanian Deadlift", "", []}
+    ]}
+  }]
+]

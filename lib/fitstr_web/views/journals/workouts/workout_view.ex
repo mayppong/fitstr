79987d816e2292f3426aa25e02028c15f442021c
May Pongpitpitak <me@mayppong.com>
2017-10-31 20:07:28 -0700

    Add a drop down for listing movement to journal

--- lib/fitstr_web/views/journals/workouts/workout_view.ex
+++ lib/fitstr_web/views/journals/workouts/workout_view.ex
@@ -1,3 +1,12 @@
 defmodule FitstrWeb.Journals.Workouts.WorkoutView do
   use FitstrWeb, :view
+
+  alias Fitstr.Activities.Workouts.Movement
+  alias Fitstr.Repo
+
+  def movements() do
+    Movement
+    |> Repo.all
+    |> Enum.map(&{&1.name, &1.id})
+  end
 end
